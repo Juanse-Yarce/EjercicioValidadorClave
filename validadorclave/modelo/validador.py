da554ea4fbce8e4228d2b5c0bce14ccee8cb7712
Juan Sebastian Lopez Yarce <jlopezyarce@gmail.com>
2024-10-17 13:39:46 -0500

    I create de class ReglaValidacion and import ABC,abstractmethod from abc

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,16 @@
 # TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod 
+
+
+class ReglaValidacion(ABC):
+
+    def __init__(self, _longitud_esperada):
+        self._longitud_esperada = _longitud_esperada
+
+    @abstractmethod
+    def es_valida(self, clave):
+        pass
+
+    
+
+
